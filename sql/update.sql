a5a68eb42c244d0b12254aaf675d653eb5f31938
Alex <aberneron@copac.fr>
2022-05-19 19:02:16 +0200

    [SQL] add: update class/sql for cars

---
+++ sql/update.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `llx_dolicars_cars` ADD `power` DOUBLE NULL AFTER `last_main_doc`;
+ALTER TABLE `llx_dolicars_cars` ADD `sport_mode` BOOLEAN NULL AFTER `power`;
+ALTER TABLE `llx_dolicars_cars` ADD `circulation_date` DATE NULL AFTER `sport_mode`;
